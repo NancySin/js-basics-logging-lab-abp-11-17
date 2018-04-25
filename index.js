3d15ca58e6a9a3317f8fe32f38ee741acfdeb635
nancy sin <nsin@sbcglobal.net>
2018-04-25 03:25:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const driver = 'bob';
 
-console.log ("this code was called");
+console.log ("this code was called")
 
-console.log ("bob");
+console.log (driver)
 
 console.log (driver, "is the driver variable name");
